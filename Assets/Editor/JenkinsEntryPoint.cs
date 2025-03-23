f5a215e9174440d2e7304f302f9d10b3d244a35f
deanryandevops <L00188387@atu.ie>
2025-03-23 00:00:45 +0000

    Getting the model file in the modeltoconvert folder and selecting the object in unity

--- Assets/Editor/JenkinsEntryPoint.cs
+++ Assets/Editor/JenkinsEntryPoint.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,9 +9,11 @@ public static class JenkinsEntryPoint
     public static void ConvertModel()
     {
         Debug.Log("Starting headless unity!");
+        string assetsPath = Application.dataPath;
+        Debug.Log("Assets Folder Path: " + assetsPath);
 
         // Define the build path
-        string modelsPath = "Assets/ModelToConvert";
+        string modelsPath = "/ModelToConvert";
 
         // Ensure the build directory exists
         if (!System.IO.Directory.Exists(modelsPath))
@@ -18,7 +22,24 @@ public static class JenkinsEntryPoint
         }
 
         Debug.Log("models path: " + modelsPath);
+        
+        string fullModelPath = System.IO.Path.Combine(assetsPath, modelsPath);
+        // Get all files in the directory
+        string[] modelFiles = Directory.GetFiles(fullModelPath);
+        string currentModelFile = modelFiles.First();
+        Debug.Log("currentModelFile: " + currentModelFile);
 
         Debug.Log("Process complete!");
+        string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(currentModelFile);
+        Debug.Log("fileNameWithoutExtension:"+fileNameWithoutExtension);
+        // Find the object by name
+        GameObject selectedObject = GameObject.Find(fileNameWithoutExtension);
+        
+        if(selectedObject == null) return;
+        
+        Selection.activeObject = selectedObject;
+        
+        selectedObject.name = "DEAN IS STAR"; // Rename the object
+        Debug.Log("Object renamed to: " + selectedObject.name);
     }
 }
